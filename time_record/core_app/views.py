d29c827ed0d9c6d96166eca2e36a6395af642d62
Jerry Yu <jeyu54217.gmail.com>
2022-07-11 15:29:02 +0800

    Update, time_record-update_or_create finished

--- time_record/core_app/views.py
+++ time_record/core_app/views.py
@@ -12,86 +12,74 @@ def home_page(request):
 @csrf_exempt
 def time_record(request):
     if request.method == "POST":
+        # general date post
         post_date = request.POST.get('date')
-        post_time_hr_0 = request.POST.get('Select_hr_0')
-        post_time_min_0 = request.POST.get('select_min_0')
-        post_time_hr_1 = request.POST.get('select_hr_1')
-        post_time_min_1 = request.POST.get('select_min_1')
+        # time records post
+        post_time_hr_s = request.POST.get('Select_hr_0')
+        post_time_min_s = request.POST.get('select_min_0')
+        post_time_hr_e = request.POST.get('select_hr_1')
+        post_time_min_e = request.POST.get('select_min_1')
         post_act_main = request.POST.get('act_type_main')
         post_act_sub = request.POST.get('act_type_sub')
         post_act_detail = request.POST.get('act_name')
         post_act_note = request.POST.get('act_note')
-        
+        # calories records post
         post_cal_MRNG = request.POST.get('cal_MRNG')
         post_cal_NOON = request.POST.get('cal_NOON')
         post_cal_NIGHT = request.POST.get('cal_NIGHT')
         post_cal_TARGET = request.POST.get('cal_TARGET')
-        if post_act_main:
+        post_cal_DIFICITE = request.POST.get('cal_DEFICITE')
+        if post_time_hr_s:
             Date_record.objects.update_or_create(
                 dt_date = post_date,
-            # defaults = {'dt_date' : date}, 
             )
-
             Act_record.objects.update_or_create(
+                # if not set defaults : filters = defaults
                 act_main = post_act_main,
                 act_sub = post_act_sub,
             )
-         
             Time_record.objects.update_or_create(
+                # filter on the unique value of below fields
                 dt_date = Date_record.objects.latest('id'),
-                act_act =  Act_record.objects.latest('id'),
-                tm_start_time = f"{post_time_hr_0}:{post_time_min_0}:00",
-                tm_end_time = f"{post_time_hr_1}:{post_time_min_1}:00",
+                tm_start_time = f"{post_time_hr_s}:{post_time_min_s}:00",
+                tm_end_time = f"{post_time_hr_e}:{post_time_min_e}:00",
+                # update these fields, or create a new object with these values
+                defaults = {
+                    'dt_date' : Date_record.objects.latest('id'),
+                    'act_act': Act_record.objects.latest('id'),
+                    'tm_start_time': f"{post_time_hr_s}:{post_time_min_s}:00",
+                    'tm_end_time': f"{post_time_hr_e}:{post_time_min_e}:00",
+                    }
             # tm_duration_hr = ,
             )
             if post_act_detail != '' or post_act_note != '':
                 Detail_record.objects.update_or_create(
+                    # filer by selected time record, not by the latest one
                     tm_time =  Time_record.objects.latest('id'),
-                    dtl_detail = post_act_detail,
-                    dtl_note = post_act_note,
-                )
+                    defaults = {
+                        'dtl_detail' : post_act_detail,
+                        'dtl_note': post_act_note,
+                        }
+                    )
             return redirect('/')
         else:
-            Calories_record.objects.update_or_create(
-                dt_date =Date_record.objects.latest('id'),
-                cal_morning = post_cal_MRNG,
-                cal_noon = post_cal_NOON,
-                cal_night = post_cal_NIGHT,
-                cal_target = post_cal_TARGET,
-                # cal_deficit = 
+            Date_record.objects.update_or_create(
+                dt_date = post_date,
             )
+            Calories_record.objects.update_or_create(
+                dt_date = Date_record.objects.get(dt_date = post_date),
+                defaults = {
+                    'dt_date' : Date_record.objects.get(dt_date = post_date),
+                    'cal_morning' : post_cal_MRNG,
+                    'cal_noon': post_cal_NOON,
+                    'cal_night': post_cal_NIGHT,
+                    'cal_target': post_cal_TARGET,
+                    'cal_deficit': post_cal_DIFICITE,
+                    }
+                )
+            return redirect('/')
 
         
-
-
-# class Calories_record(models.Model):
-#     dt_date = models.OneToOneField(
-#         Date_record,
-#         on_delete = models.CASCADE,
-#     )
-#     cal_morning = models.IntegerField(
-#         blank = True,
-#         null = True,
-#     )
-#     cal_noon = models.IntegerField(
-#         blank = True,
-#         null = True,
-#     )
-#     cal_night = models.IntegerField(
-#         blank = True,
-#         null = True,
-#     )
-#     cal_target = models.IntegerField(
-#         blank = True,
-#         null = True,
-#     )
-#     cal_deficit = models.IntegerField(
-#         blank = True,
-#         null = True,
-#     )
-    
-
-
 
 
 @csrf_exempt
